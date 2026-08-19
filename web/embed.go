e01a39bddd9fa798b8313a9aa7b4f5e64e42248c
aguszunda <38926980+aguszunda@users.noreply.github.com>
2026-08-19 12:45:03 -0300

    fix(auth): ocultar menu en login/registro y centrar boton de ingreso (#7)
    * fix(auth): ocultar menu en login/registro y centrar boton de ingreso
    
    * refactor(web): mover estilos css a archivos estaticos embebidos
    
    * refactor(web): mover estilos css a archivos estaticos embebidos

--- web/embed.go
+++ web/embed.go
@@ -8,8 +8,19 @@ import (
 //go:embed templates/*.html
 var templateFiles embed.FS
 
+//go:embed static
+var staticFiles embed.FS
+
 func TemplateFS() fs.FS {
 	sub, err := fs.Sub(templateFiles, "templates")
+	if err != nil {
+		panic(err)
+	}
+	return sub
+}
+
+func StaticFS() fs.FS {
+	sub, err := fs.Sub(staticFiles, "static")
 	if err != nil {
 		panic(err)
 	}
