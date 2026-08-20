1ef9bbc67061c242a695bcde0924bd833fe0e89f
aguszunda <agustin.zunda@gmail.com>
2026-08-20 17:19:48 -0300

    feat(auth): diferenciar errores de login y mostrar modal de error

--- internal/model/errors.go
+++ internal/model/errors.go
@@ -3,12 +3,12 @@ package model
 import "errors"
 
 var (
-	ErrNotFound     = errors.New("recurso no encontrado")
-	ErrUnauthorized = errors.New("no autorizado")
-	ErrInvalidInput = errors.New("entrada inválida")
-	ErrDuplicate    = errors.New("el recurso ya existe")
-	ErrMesCerrado   = errors.New("el mes está cerrado, no se puede modificar")
-	ErrForbidden    = errors.New("acceso denegado")
-	ErrEmailExiste  = errors.New("el email ya está registrado")
+	ErrNotFound      = errors.New("recurso no encontrado")
+	ErrUnauthorized  = errors.New("no autorizado")
+	ErrInvalidInput  = errors.New("entrada inválida")
+	ErrDuplicate     = errors.New("el recurso ya existe")
+	ErrMesCerrado    = errors.New("el mes está cerrado, no se puede modificar")
+	ErrForbidden     = errors.New("acceso denegado")
+	ErrEmailExiste   = errors.New("el email ya está registrado")
 	ErrEmailNoExiste = errors.New("el email no está registrado")
 )
